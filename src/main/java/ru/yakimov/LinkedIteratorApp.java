114f1d8f20210e84360830b0ebb36ead91e465f7
Vladimir Iakimov <yakimovvn@bk.ru>
2019-08-20 00:07:41 +0300

    Create LinkedIteratorApp and write incite methods for testing

--- src/main/java/ru/yakimov/LinkedIteratorApp.java
+++ src/main/java/ru/yakimov/LinkedIteratorApp.java
@@ -2,38 +2,66 @@ package ru.yakimov;
 
 
 import ru.yakimov.BidirectionalList.BidirectionalListImpl;
+import ru.yakimov.LinkedList.LinkedList;
+import ru.yakimov.LinkedList.LinkedListImpl;
 
-import java.sql.SQLOutput;
 import java.util.Iterator;
 
 public class LinkedIteratorApp {
 
     public static void main(String[] args) {
-        BidirectionalListImpl<Integer> brl = new BidirectionalListImpl<>();
-        brl.insertFirst(1);
-        brl.insertFirst(2);
-        brl.insertFirst(3);
-        brl.insertFirst(4);
+        System.out.println("Check BidirectionalListImpl");
+        checkBidirectionalListImplIterator(getBidirectionalListImpl());
+
+        System.out.println("Check LinkedListImpl");
+        checkBidirectionalListImplIterator(getLinkedListImpl());
+    }
+
+    private static <T extends LinkedList<Integer>> void checkBidirectionalListImplIterator(T collection) {
 
         System.out.println("Check remove, hasNext, next");
 
-        brl.display();
+        collection.display();
 
-        Iterator<Integer> iterator = brl.iterator();
+        Iterator iterator = collection.iterator();
         while (iterator.hasNext()){
-            int i = iterator.next();
-            if(i == 3)
+            var i = iterator.next();
+            if(i.equals(3))
                 iterator.remove();
         }
 
-        brl.display();
+        collection.display();
 
-        System.out.println("Check foreach");
+        System.out.println("Check foreach cycle");
 
-        for (Integer integer : brl) {
-            System.out.println(integer);
+        for (var i : collection) {
+            System.out.println(i);
         }
 
-        brl.forEach(System.out::println);
+        System.out.println("Check foreach method");
+
+        collection.forEach(System.out::println);
+
+        System.out.println("Check foreachRemaining iterators method");
+
+        collection.iterator().forEachRemaining(System.out::println);
+    }
+
+    private static BidirectionalListImpl<Integer> getBidirectionalListImpl() {
+        BidirectionalListImpl<Integer> brl = new BidirectionalListImpl<>();
+        addTestValues(brl,4,3,2,1);
+        return brl;
+    }
+
+    private static LinkedListImpl<Integer> getLinkedListImpl() {
+        LinkedListImpl<Integer> lli = new LinkedListImpl<>();
+        addTestValues(lli,1,2,3,4);
+        return lli;
+    }
+
+    private static <T extends LinkedList<Integer>> void addTestValues(T collection, int ... numbArr){
+        for (var i : numbArr) {
+            collection.insertFirst(i);
+        }
     }
 }
